626bf49179834956107c9b51ae542337869847b3
sjlee9908 <sjlee9908@naver.com>
2026-07-28 16:01:05 +0900

    .

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-cd /app/darknet_ver
+cd ~
 gcc -Wall -O2 -Ilib/darknet/include -Ilib/darknet/src -Isrc \
     main.c src/util.c src/queue.c \
     $(printf 'lib/darknet/src/%s.c ' gemm utils cuda deconvolutional_layer convolutional_layer \
--- onnx_ver/init.sh
+++
@@ -1,6 +0,0 @@
-apt update -y
-
-apt upgrade -y
-
-apt install -y git
-
